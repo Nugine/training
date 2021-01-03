6a45f485a81fcbac890fe495f27fc52e03aefce0
Nugine <nugine@foxmail.com>
2021-01-03 14:58:24 +0800

    load questions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use anyhow::Result;
+use once_cell::sync::Lazy;
+use training::Question;
+
+static QUESTIONS: Lazy<Vec<Question>> = Lazy::new(|| {
+    const QUESTIONS_JSON: &str = include_str!("../data/questions.json");
+    serde_json::from_str(QUESTIONS_JSON).unwrap()
+});
 
 fn main() -> Result<()> {
+    dbg!(QUESTIONS.len());
     Ok(())
 }
